9ef3d47beef5b14126f7bff8868dbf9094d9a139
RiczCalixto <rcalixtos@gmail.com>
2022-01-04 10:05:58 -0300

    feat: complete exercise 1 and 2

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -31,7 +31,7 @@ function App() {
   return (
     <main className={styles.characterEditor}>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
-        <header className={styles.header}>
+        <header  className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
             Customize your character's look and style using the
